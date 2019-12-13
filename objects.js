b4984270677835b59653abbb2d2b2b3c816ee5fc
Hannah Drury <hannah_drury@yahoo.com>
2019-12-13 22:25:01 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist = {artistName: 'songTitle'};
 
 function updatePlaylist(playlist, artistName, songTitle)
 {
-  artistName.songTitle = 'Phil Ochs';
+  artistName['Phil Ochs'] = 'song1'
   return playlist
 }
 
