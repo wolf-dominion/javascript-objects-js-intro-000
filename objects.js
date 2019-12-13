880294e6702b57c102427e432b6527b8055f5037
Hannah Drury <hannah_drury@yahoo.com>
2019-12-13 22:15:56 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist = {artistNames: 'song titles'};
 
 function updatePlaylist(obj, artist, songTitle)
 {
-  
+  artistNames.song = 'new song';
 }
 
 function removeFromPlaylist(obj, artist)
